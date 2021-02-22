f7bf6036da58dfd0524608fd3643b3f0dd93ab84
EikESousA <eike.sousa@hotmail.com>
2021-02-22 13:45:27 -0300

    aula01

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from '../components';
+
+import '../styles/global.css';
+import '../styles/colors.css';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <ExperienceBar></ExperienceBar>
+    </div>
+  );
+}
